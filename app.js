bdf53b5ed686fd8b4a48d58ed6608d6204aebb1f
alemaxi <alelogovir@gmail.com>
2021-09-05 10:06:55 -0300

    First project commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('**', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use('/teste', indexRouter);
+
+
+
+module.exports = app;
